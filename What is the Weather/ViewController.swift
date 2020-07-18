2ddd561c49d6cce14be9ac4445eab646178a805b
Jeremy Hou <jeremyrhou@gmail.com>
2020-07-17 23:14:40 -0400

    Initial Commit

---
+++ What is the Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What is the Weather
+//
+//  Created by Jeremy on 7/17/20.
+//  Copyright © 2020 Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
