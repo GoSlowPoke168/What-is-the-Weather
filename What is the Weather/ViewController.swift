f976f71fbaf3ad83dc2bbb67efad7a126fa63507
Jeremy Hou <jeremyrhou@gmail.com>
2020-07-17 23:56:46 -0400

    Baseline

--- What is the Weather/ViewController.swift
+++ What is the Weather/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
+    @IBAction func submitButtonClicked(_ sender: Any) {
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(textField.text!.replacingOccurrences(of: " ", with: "%20"))&appid=7ba1c689565875c7fe33589666bf7afc") {
+                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if error != nil {
+                        print("Error")
+                    } else {
+                        if let urlContent = data {
+                            do {
+                                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                                print(jsonResult)
+                                if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String{
+                                    DispatchQueue.main.sync(execute: {
+                                    self.outputLabel.text = description
+                                })
+                            }
+                        } catch {
+                            print("JSON processing failed")
+                        }
+                    }
+                    
+                }
+            }
+            task.resume()
+        } else {
+            textField.text = "Couldn't find weather in that city. Please try again"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
+
+
+
 
